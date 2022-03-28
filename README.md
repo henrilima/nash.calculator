1bb09dd4e3118c8c0cc2dbc2b8f5e824ff13cb2b
Henri Lima <henrilima.contactme@gmail.com>
2022-03-27 21:28:09 -0300

    last commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Calculator
+
+### <b>View:</b> [clique aqui](https://nashcompany.cf/global/);
+ 
+ <br/>
+
+ # Screenshots
+
+### Desktop Version:
+
+> ![Desktop](./assets/screen.png)
+And nothing else. :D
---
+++ javascript/main.js
@@ -0,0 +1,69 @@
+var symbol = 0;
+
+function clean() {
+    var last = document.getElementById("last-calc");
+    var result = document.getElementById("result");
+
+    last.innerText = "0";
+    result.innerText = "0";
+    symbol = 0;
+    firstSymbol = true;
+}
+
+// Max = 19
+function acr(string, type) {
+    if (type === "symbol") {
+        isSymbol(string);
+    }
+
+    var result = document.getElementById("result");
+
+    if (result.innerText.length > 16) {
+        return alert("O limite máximo é de 17 caracteres.");
+    }
+
+    if (result.innerText === "0") {
+        result.innerText = "";
+    } else if (result.innerText === "0" && type === "symbol") {
+        return (result.innerText = "0");
+    }
+
+    if (type !== "symbol") {
+        result.innerText += String(string);
+    }
+}
+
+function total() {
+    var last = document.getElementById("last-calc");
+    var result = document.getElementById("result");
+
+    last.innerText = result.innerText;
+    try {
+        result.innerText = String(eval(result.innerText)).slice(0, 17);
+    } catch (e) {
+        alert("Não foi possível calcular, verifique a conta!");
+    }
+}
+
+function removeLast() {
+    var result = document.getElementById("result");
+    var list = result.innerText.split("");
+    list.splice(-1, 1);
+
+    result.innerText = list.join().replace(/\,/g, "");
+    result = document.getElementById("result");
+    if (result.innerText === "") result.innerHTML = "0";
+}
+
+function isSymbol(string) {
+    var result = document.getElementById("result");
+    var list = result.innerText.split("");
+    var idk = list.pop();
+
+    if (["/", "*", "-", "+"].includes(idk)) {
+        removeLast();
+        acr(string);
+    } else {
+        acr(string);
+    }
+}
